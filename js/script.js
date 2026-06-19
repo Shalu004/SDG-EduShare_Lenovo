201c32c8689131cd71fb354f510528019c8ecf6a
sapty_ <mushaluthakur785@gmail.com>
2026-06-19 12:40:02 +0530

    Implement search functionality for resource cards
    Added a searchResource function to filter cards based on user input.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log("EduShare Loaded Successfully");
+function searchResource() {
+
+    let input =
+        document.getElementById("search")
+        .value.toLowerCase();
+
+    let cards =
+        document.querySelectorAll(".card");
+
+    cards.forEach(card => {
+
+        let text =
+            card.innerText.toLowerCase();
+
+        if (text.includes(input))
+            card.style.display = "block";
+        else
+            card.style.display = "none";
+
+    });
+}
